6b5da25bd782c8b655fe8a456b573bbd10d403af
Sana E <142530591+Sanae0312@users.noreply.github.com>
2024-12-17 09:52:13 +0530

    Update prg1.5.cpp

--- Day29/prg01/prg1.5.cpp
+++ Day29/prg01/prg1.5.cpp
@@ -13,8 +13,8 @@ class Employee
         unique_ptr<string> name;
 
     public:
-        Employee(int v_id = 0, int v_age = 0, string v_name = "")
-            : id(make_unique<int>(v_id)), age(make_unique<int>(v_age)), name(make_unique<string>(v_name)) {}
+        Employee(int e_id = 0, int e_age = 0, string e_name = "")
+            : id(make_unique<int>(e_id)), age(make_unique<int>(e_age)), name(make_unique<string>(e_name)) {}
 
     virtual void printDetails() const 
     {
@@ -28,8 +28,8 @@ class Employee
         std::swap(name, other.name);
     }
 
-    friend ostream& operator<<(ostream& os, const Employee& e);
-    friend istream& operator>>(istream& is, Employee& e);
+    friend ostream& operator<<(ostream& out, const Employee& employee);
+    friend istream& operator>>(istream& input, Employee& employee);
 };
 
 class Programmer : public Employee
@@ -39,14 +39,14 @@ class Programmer : public Employee
         unique_ptr<int> taskCount;
 
     public:
-        Programmer(int v_id = 0, int v_age = 0, string v_name = "", string* v_tasks = nullptr, int v_taskCount = 0)
-            : Employee(v_id, v_age, v_name), taskCount(make_unique<int>(v_taskCount)) 
+        Programmer(int p_id = 0, int p_age = 0, string p_name = "", string* p_tasks = nullptr, int p_taskCount = 0)
+            : Employee(p_id, p_age, p_name), taskCount(make_unique<int>(p_taskCount)) 
             {
                 tasks = make_unique<string[]>(*taskCount);
-                if (v_tasks) {
+                if (p_tasks) {
                     for (int i = 0; i < *taskCount; ++i) 
                     {
-                        tasks[i] = v_tasks[i];
+                        tasks[i] = p_tasks[i];
                     }
             }
     }
@@ -68,53 +68,53 @@ class Programmer : public Employee
         }
     }
 
-    friend ostream& operator<<(ostream& os, const Programmer& p);
-    friend istream& operator>>(istream& is, Programmer& p);
+    friend ostream& operator<<(ostream& out, const Programmer& programmer);
+    friend istream& operator>>(istream& input, Programmer& programmer);
 };
 
 // Overloaded operator<< for Employee
-ostream& operator<<(ostream& os, const Employee& e) 
+ostream& operator<<(ostream& out, const Employee& employee) 
 {
-    e.printDetails();
-    return os;
+    employee.printDetails();
+    return out;
 }
 
 // Overloaded operator>> for Employee
-istream& operator>>(istream& is, Employee& e) 
+istream& operator>>(istream& input, Employee& employee) 
 {
     cout << "Enter ID: ";
-    is >> *e.id;
+    input >> *employee.id;
     cout << "Enter Age: ";
-    is >> *e.age;
+    input >> *employee.age;
     cout << "Enter Name: ";
-    is.ignore();
-    getline(is, *e.name);
-    return is;
+    input.ignore();
+    getline(input, *employee.name);
+    return input;
 }
 
 // Overloaded operator<< for Programmer
-ostream& operator<<(ostream& os, const Programmer& p) 
+ostream& operator<<(ostream& out, const Programmer& programmer) 
 {
-    p.printDetails();
-    return os;
+    programmer.printDetails();
+    return out;
 }
 
 // Overloaded operator>> for Programmer
-istream& operator>>(istream& is, Programmer& p) 
+istream& operator>>(istream& input, Programmer& programmer) 
 {
-    is >> static_cast<Employee&>(p);  // Input base class details
+    input >> static_cast<Employee&>(programmer);  // Input base class details
     cout << "Enter number of tasks: ";
     int count;
-    is >> count;
-    p.taskCount = make_unique<int>(count);
-    p.tasks = make_unique<string[]>(count);
+    input >> count;
+    programmer.taskCount = make_unique<int>(count);
+    programmer.tasks = make_unique<string[]>(count);
     cout << "Enter tasks:\n";
-    is.ignore();  // Clear buffer
+    input.ignore();  // Clear buffer
     for (int i = 0; i < count; ++i) {
         cout << "Task " << i + 1 << ": ";
-        getline(is, p.tasks[i]);
+        getline(input, programmer.tasks[i]);
     }
-    return is;
+    return input;
 }
 
 int main() 
