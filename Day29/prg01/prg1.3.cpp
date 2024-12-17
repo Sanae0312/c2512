0d06e4385ca4077ea2851dbf05ba244a44a4721a
Sana E <142530591+Sanae0312@users.noreply.github.com>
2024-12-17 10:48:36 +0530

    Update prg1.3.cpp

--- Day29/prg01/prg1.3.cpp
+++ Day29/prg01/prg1.3.cpp
@@ -10,91 +10,104 @@
 #include <string>
 using namespace std;
 
-class Employee {
-public:
-    int* id;         
-    int* age;         
-    string* name; 
+class Employee 
+{
+   public:
+       int* id;         
+       int* age;         
+       string* name; 
+   
+   public:
+       Employee(int e_id, int e_age, string e_name)
+       {
+           id = new int(e_id);          
+           age = new int(e_age);        
+           name = new string(e_name);  
+       }
 
-public:
-    Employee(int v_id, int v_age, string v_name) {
-        id = new int(v_id);          
-        age = new int(v_age);        
-        name = new string(v_name);  
-    }
-
-    virtual ~Employee() {
+    virtual ~Employee() 
+    {
         delete id;    
         delete age;   
         delete name; 
     }
 
-    virtual void printDetails() {
+    virtual void printDetails()
+   {
         cout << *id << " " << *age << " " << *name; 
     }
 
-    virtual void swap(Employee& other) {
+    virtual void swap(Employee& other)
+    {
         std::swap(id, other.id);  
         std::swap(age, other.age);
         std::swap(name, other.name);
     }
 
-    friend ostream& operator<<(ostream& os,Employee& );
+    friend ostream& operator<<(ostream& out,Employee& employee);
 };
 
 
-class Programmer : public Employee {
-public:
-    string* tasks;  
-    int taskCount;  
+class Programmer : public Employee 
+{
+   public:
+       string* tasks;  
+       int taskCount;  
+   
+   public:
+       Programmer(int p_id, int p_age, string p_name, string* p_tasks, int p_taskCount)
+           : Employee(p_id, p_age, p_name), taskCount(p_taskCount)
+       {
+           tasks = new string[taskCount];   
+           for (int i = 0; i < taskCount; i++) 
+           {
+               tasks[i] = p_tasks[i];  
+           }
+       }
 
-public:
-    Programmer(int p_id, int p_age, string p_name, string* p_tasks, int p_taskCount)
-        : Employee(p_id, p_age, p_name), taskCount(p_taskCount) {
-        tasks = new string[taskCount];   
-        for (int i = 0; i < taskCount; i++) {
-            tasks[i] = p_tasks[i];  
-        }
-    }
-
-    ~Programmer() {
+    ~Programmer() 
+    {
         delete[] tasks;
     }
 
-    void swap(Programmer& other){
+    void swap(Programmer& other)
+    {
         std::swap(taskCount, other.taskCount);
         std::swap(tasks, other.tasks);
         
         Employee::swap(other);
     }
 
-    void printDetails() override {
+    void printDetails() override 
+    {
         Employee::printDetails(); 
         cout << " Tasks: ";
-        for (int i = 0; i < taskCount; i++) {
+        for (int i = 0; i < taskCount; i++)
+        {
             cout << tasks[i]<<" ";  
         }
     }
 
-    friend ostream& operator<<(ostream& os, Programmer& );
+    friend ostream& operator<<(ostream& out, Programmer& programmer);
 };
 
-ostream& operator<<(ostream& os, Employee& e) {
-    e.printDetails();
-    return os; 
+ostream& operator<<(ostream& out, Employee& employee) 
+{
+    employee.printDetails();
+    return out; 
 }
 
-ostream& operator<<(ostream& os,Programmer& p) {
-    p.printDetails();
-    return os;  
+ostream& operator<<(ostream& out,Programmer& programmer) 
+{
+    programmer.printDetails();
+    return out;  
 }
 
-int main() {
-    // Create task array for Programmer 1
+int main() 
+{
     string tasks1[] = {"Task10", "Task11", "Task12"};
     Employee* p1 = new Programmer(101, 22, "Athira", tasks1, 3);
 
-    // Create task array for Programmer 2
     string tasks2[] = {"Task20", "Task21", "Task22"};
     Employee* p2 = new Programmer(102, 23, "Bhagya", tasks2, 3);
 
